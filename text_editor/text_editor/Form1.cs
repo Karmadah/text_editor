abcebba3621a9902c2358998efa13f25660e2751
Karmada1 <63024817+Karmada1@users.noreply.github.com>
2021-01-11 21:39:22 -0800

    INIT

---
+++ text_editor/text_editor/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace text_editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b_save_text_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, rtb_main_text.Text);
+            }
+        }
+
+        private void b_open_text_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                rtb_main_text.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+    }
+}
